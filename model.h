59b3c35d124aeb25b6e285318775b5bcb50853e0
Baydin55 <baydin55@example.com>
2025-04-14 00:34:19 +0300

    new updates

--- model.h
+++ model.h
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-#ifndef MODEL_H
-#define MODEL_H
-
-#include <gtk/gtk.h>
-#include <semaphore.h>
-#include <sys/types.h>
-#include <stddef.h>
-
-// Paylaşılan bellek yapısı
-#define MSG_BUF_SIZE 4096
-#define MAX_COMMAND_LENGTH 512
-
-typedef struct {
-    sem_t sem;
-    int fd;
-    char msgbuf[MSG_BUF_SIZE];
-    size_t cnt;
-} ShmBuf;
-
-typedef struct {
-    pid_t pid;
-    char command[MAX_COMMAND_LENGTH];
-    int status;
-} ProcessInfo;
-
-
-// Fonksiyon prototipleri
-ShmBuf *buf_init();
-void execute_command(const char *cmd, ShmBuf *shmp, GtkTextBuffer *text_buffer);
-
-void send_message(ShmBuf *shmp, const char *msg);
-void receive_message(ShmBuf *shmp);
-
-#endif // MODEL_H
-
-
-
-
-
-=======
 #ifndef MODEL_H
 #define MODEL_H
 
@@ -82,4 +41,3 @@ ProcessInfo *get_process_history();
 int get_history_count();
 
 #endif // MODEL_H
->>>>>>> master
